3711770678557a88023f4b3af447e23b98075139
Alexey Gulev <me@agulev.com>
2021-09-25 19:23:42 +0200

    Use another method for uptime on iOS and MacOS

--- uptime/src/UpTime_ios.mm
+++ uptime/src/UpTime_ios.mm
@@ -1,9 +1,22 @@
 #if defined(DM_PLATFORM_IOS) || defined(DM_PLATFORM_OSX)
-#include <AVFoundation/AVFoundation.h>
+
+#include <sys/sysctl.h>  
 
 uint32_t UpTime_get()
 {
-  return [[NSProcessInfo processInfo] systemUptime];
+  struct timeval boottime;
+  int mib[2] = {CTL_KERN, KERN_BOOTTIME};
+  size_t size = sizeof(boottime);
+  time_t now;
+  time_t uptime = -1;
+
+  (void)time(&now);
+
+  if (sysctl(mib, 2, &boottime, &size, NULL, 0) != -1 && boottime.tv_sec != 0) {
+    uptime = now - boottime.tv_sec;
+    return uptime;
+  }
+  return 0;
 }
 
 #endif
